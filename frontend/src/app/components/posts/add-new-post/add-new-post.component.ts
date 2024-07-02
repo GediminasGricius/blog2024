abacae28b79a8fe448a8653fbb6bed2cd831c5be
GediminasGricius <88775405+GediminasGricius@users.noreply.github.com>
2024-07-02 10:35:59 +0300

    Dialogai, pridėjimas

--- frontend/src/app/components/posts/add-new-post/add-new-post.component.ts
+++ frontend/src/app/components/posts/add-new-post/add-new-post.component.ts
@@ -5,6 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
 import { FormsModule, NgForm } from '@angular/forms';
+import { PostsService } from '../../../services/posts.service';
 
 @Component({
   selector: 'app-add-new-post',
@@ -14,17 +15,32 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './add-new-post.component.css'
 })
 export class AddNewPostComponent {
-  constructor (private form:MatDialogRef<AddNewPostComponent>){
+  constructor (private form:MatDialogRef<AddNewPostComponent>, private postsService:PostsService){
 
   }
 
   public onCloseForm(){
-    this.form.close();
+    this.form.close(false);
 
   }
 
   public onSubmitPost(form:NgForm){
     console.log(form.form.value);
+    this.postsService.addPost({
+      title:form.form.value.title,
+      content:form.form.value.content,
+      author:{
+        name:form.form.value.author_name,
+        email:form.form.value.author_email
+      },
+      comments:[]
+    }).subscribe({
+      next:(result)=>{
+        //console.log(result);
+        this.form.close(true);
+      }
+    })
+    
   } 
 
 }
--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,13 @@ import express, { Application } from "express";
 import mongoose from "mongoose";
 import { Post } from "./models/post";
 import { postsRouter } from "./routes/posts.router";
+import { corsHeaders } from "./middlewares/cors.middleware";
 
 
 const app:Application=express();
 
 // mongodb://localhost:27017/
+app.use(corsHeaders);
 
 mongoose.connect("mongodb://localhost:27017/blog")
     .then(()=>{
@@ -15,6 +17,7 @@ mongoose.connect("mongodb://localhost:27017/blog")
     .catch((error)=>console.log(error));
 
 app.use(express.json());
+
 
 app.use("/posts", postsRouter);    
 
